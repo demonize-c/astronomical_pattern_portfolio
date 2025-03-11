73919af6c3addaa648a0f3a2db1192848dd28713
demonize-c <sourabmajhi.200@gmail.com>
2025-03-11 21:50:05 +0530

    slow_fast_slow_bugfix

--- assets/dist/js/script.js
+++ assets/dist/js/script.js
@@ -9,6 +9,60 @@ function get_y(deg, rad){
     return "calc(sin("+ deg.toFixed(2) + "deg)*" + rad + " + 50%)";
 }
 
+
+function slowFastSlowRange(begin, end, total_points){
+      
+   let range_len = Math.abs( end - begin);//calculated the range length unit taken as 1
+
+   /**
+    * If we find range length is greater than passed length,
+    * then we can`t scale the points to be fall into ranges like (0,1) or (1,0).
+    */
+   if(range_len > total_points){
+      throw Error(`range length-${range_len} is greater than passed length-${total_points}`);
+   }
+   /**
+    *  Here it`s unit of the generated output range.
+    */
+   let unit_range = range_len/total_points;
+   /**
+    * If the points between range (0,$len) become scaled into the points 
+    * between range (0,PI/2). Then 1 unit become to this unit.
+    **/
+   let unit_degree  = (Math.PI) / (total_points - 1); // in radian
+   let output_range = [];
+
+   /**
+    * Here we first scale the points between (0, $len) to the points between (0, PI/2).
+    * Then we passed the radian points to the COS function that scaled input points between
+    * range (1,0).After that we get the scaled points back from it. 
+    * Then we tried to get the points between the range (0,$range_len).
+    */
+   for (let i = 0; i < total_points; i++) {
+      let radian_point        = unit_degree * i;
+      let normalized_point    = parseFloat((Math.cos(radian_point)/2).toFixed(3))
+      let scaled_point        = (normalized_point + 0.5) * total_points;
+      let translated_point    = scaled_point * unit_range;
+      let reverse_translated_point = range_len - translated_point;
+      let original_point     = parseFloat((reverse_translated_point + begin).toFixed(3));
+      output_range.push(original_point);
+   }
+   return output_range;
+}
+
+function slowFastSlowRangeMod(begin, end, total_points){
+
+   let sfs_range = []; //sfs stands slow fast slow
+   if(begin > end){
+   sfs_range = slowFastSlowRange(end, begin, total_points);
+   sfs_range.reverse();
+   }else{
+   sfs_range = slowFastSlowRange(begin, end, total_points);
+   }
+   return sfs_range;
+}
+
+
 function filter_deg_clockwise( deg, add){
    let eval = deg + add;
    if(eval < 0){
@@ -16,7 +70,7 @@ function filter_deg_clockwise( deg, add){
    }
    return eval;
 }
-
+/*
 function rotate_menu_to(that){
    let btn_deg = parseFloat($(that).attr('deg'));
    let diff_deg;
@@ -69,7 +123,7 @@ function rotate_menu_to(that){
    })
 // console.log(diff_deg)
 }
-
+*/
 $(document).ready(function(){
 
     // var i=0;
@@ -119,48 +173,13 @@ $(document).ready(function(){
    // }
 
 
-   function slowFastSlowRange(begin, end, total_points){
-      
-      let range_len = Math.abs( end - begin);//calculated the range length unit taken as 1
-
-      /**
-       * If we find range length is greater than passed length,
-       * then we can`t scale the points to be fall into ranges like (0,1) or (1,0).
-       */
-      if(range_len > total_points){
-         throw Error(`range length-${range_len} is greater than passed length-${total_points}`);
-      }
-      /**
-       *  Here it`s unit of the generated output range.
-       */
-      let unit_range = range_len/total_points;
-      /**
-       * If the points between range (0,$len) become scaled into the points 
-       * between range (0,PI/2). Then 1 unit become to this unit.
-       **/
-      let unit_degree  = (Math.PI/2) / total_points; // in radian
-      let output_range = [];
-
-      /**
-       * Here we first scale the points between (0, $len) to the points between (0, PI/2).
-       * Then we passed the radian points to the COS function that scaled input points between
-       * range (1,0).After that we get the scaled points back from it. 
-       * Then we tried to get the points between the range (0,$range_len).
-       */
-      for (let i = 1; i <= total_points; i++) {
-         let radian_point        = unit_degree * i;
-         let normalized_point    = parseFloat(Math.cos(radian_point).toFixed(3));
-         let scaled_point        = normalized_point * total_points;
-         let translated_point    = scaled_point * unit_range;
-         let reverse_translated_point = range_len - translated_point;
-         let original_point     = parseFloat((reverse_translated_point + begin).toFixed(3));
-         output_range.push(original_point);
-      }
-      return output_range;
-   }
+   //50 0
+   
 
 
-   console.log(slowFastSlowRange(50,150,100));
+
+    console.log(slowFastSlowRangeMod(50,0,50));
+    
 
     // $('.btn-circle').click(function(){
     //     // var index = $(this).index();
